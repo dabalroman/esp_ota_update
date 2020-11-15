fefc4bc2ecc7c8660a598b61f8eb642214229512
Roman Dąbal <dabalroman@gmail.com>
2020-11-15 21:03:46 +0100

    Spring init

---
+++ server/src/main/java/com/esp_ota_update/server/EspOtaUpdateServerApplication.java
@@ -0,0 +1,13 @@
+package com.esp_ota_update.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EspOtaUpdateServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EspOtaUpdateServerApplication.class, args);
+    }
+
+}
---
+++ server/src/test/java/com/esp_ota_update/server/EspOtaUpdateServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esp_ota_update.server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EspOtaUpdateServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
