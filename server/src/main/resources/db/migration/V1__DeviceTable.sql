9b2a9f2da291b9d0bb2b894e89270df0f164eab8
Roman Dąbal <dabalroman@gmail.com>
2020-11-19 19:57:31 +0100

    Basic SQL scheme

--- server/src/main/resources/db/migration/V1__DeviceTable.sql
+++ server/src/main/resources/db/migration/V1__DeviceTable.sql
@@ -1,5 +1,41 @@
-CREATE TABLE device (
-    id varchar(36) NOT NULL PRIMARY KEY,
-    name varchar(128) NOT NULL
+CREATE TABLE `device`
+(
+    `id`                   int PRIMARY KEY AUTO_INCREMENT,
+    `mac`                  varchar(17),
+    `name`                 varchar(128) NOT NULL,
+    `software_name_scheme` varchar(128),
+    `status`               int,
+    `last_checked`         datetime     NOT NULL,
+    `last_updated`         datetime     NOT NULL
 );
 
+CREATE TABLE `update`
+(
+    `id`            int PRIMARY KEY AUTO_INCREMENT,
+    `timestamp`     datetime NOT NULL,
+    `device_id`     int,
+    `software_from` int,
+    `software_to`   int
+);
+
+CREATE TABLE `software`
+(
+    `id`         int PRIMARY KEY AUTO_INCREMENT,
+    `version`    varchar(32) NOT NULL,
+    `path`       varchar(128),
+    `md5`        varchar(32),
+    `created_at` datetime,
+    `device_id`  int
+);
+
+ALTER TABLE `update`
+    ADD FOREIGN KEY (`device_id`) REFERENCES `device` (`id`);
+
+ALTER TABLE `update`
+    ADD FOREIGN KEY (`software_from`) REFERENCES `software` (`id`);
+
+ALTER TABLE `update`
+    ADD FOREIGN KEY (`software_to`) REFERENCES `software` (`id`);
+
+ALTER TABLE `software`
+    ADD FOREIGN KEY (`device_id`) REFERENCES `device` (`id`);
