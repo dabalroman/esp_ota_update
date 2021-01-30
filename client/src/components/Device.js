3cbd106e6ace2ca5e790c7c131a7cbb1014dc886
Roman Dąbal <dabalroman@gmail.com>
2021-01-30 21:44:47 +0100

    Device class

--- client/src/components/Device.js
+++ client/src/components/Device.js
@@ -1,7 +1,53 @@
-export default class Device {
-  static statusMap = ['New', 'Up to date', 'Needs update', 'No software'];
+import DateUtility from '@/utils/DateUtility';
 
-  static mapStatusToString (status) {
-    return Device.statusMap[status];
+export default class Device {
+  statusMap = ['New', 'Up to date', 'Needs update', 'No software', 'Update in progress'];
+  statusClassMap = ['text-primary', 'text-success', 'text-warning', 'text-danger', 'text-warning'];
+
+  constructor (id, name, mac, softwareNameScheme, softwareVersion, status, lastSoftwareCheck, lastSoftwareUpdate) {
+    this._id = id;
+    this._name = name;
+    this._mac = mac;
+    this._softwareNameScheme = softwareNameScheme;
+    this._status = status;
+    this._softwareVersion = softwareVersion;
+    this._lastSoftwareCheck = lastSoftwareCheck;
+    this._lastSoftwareUpdate = lastSoftwareUpdate;
+  }
+
+  get id () {
+    return this._id;
+  }
+
+  get name () {
+    return this._name;
+  }
+
+  get mac () {
+    return this._mac;
+  }
+
+  get softwareNameScheme () {
+    return this._softwareNameScheme;
+  }
+
+  get status () {
+    return this.statusMap[this._status];
+  }
+
+  get softwareVersion () {
+    return this._softwareVersion ?? '-';
+  }
+
+  get lastSoftwareCheck () {
+    return DateUtility.formatDateString(this._lastSoftwareCheck);
+  }
+
+  get lastSoftwareUpdate () {
+    return DateUtility.formatDateString(this._lastSoftwareUpdate);
+  }
+
+  get statusClass () {
+    return this.statusClassMap[this._status];
   }
 }
